7c89d64fe2b68fea3aa158f4775d01440c8e8e5b
raissarramos9-stack <raissarramos9@gmail.com>
2026-03-09 16:46:27 +0000

    Lição 2.1 - Posicionamento do Jogador

---
+++ Assets/Scenes/Prototype 2/PlayerController2.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 20.0f;
+    public float xRange = 20;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Keep the player in bounds
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
