69de7dabecaba611dba3874b1b40f3fc12b6d409
raissarramos9-stack <raissarramos9@gmail.com>
2026-03-09 17:15:25 +0000

    Lição 2.2 - Voo de Comida

--- Assets/script/PlayerController2.cs
+++ Assets/script/PlayerController2.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerController2 : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 20.0f;
-    public float xRange = 20;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
+    public GameObject projectilePrefab;
 
     void Update()
     {
@@ -13,7 +15,6 @@ public class PlayerController2 : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         // Keep the player in bounds
-
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -22,6 +23,11 @@ public class PlayerController2 : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
