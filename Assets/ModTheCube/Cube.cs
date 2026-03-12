2d724e2c77f99a8da6a922c27d281e94fb82018c
raissarramos9-stack <raissarramos9@gmail.com>
2026-03-12 13:48:08 +0000

    Modificar o Cubo

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float rotationSpeed = 60f;
+    public float scaleSpeed = 2f;
+
+    Renderer cubeRenderer;
+
+    void Start()
+    {
+        cubeRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        // girar o cubo
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        // mudar tamanho
+        float scale = 1 + Mathf.Sin(Time.time * scaleSpeed) * 0.3f;
+        transform.localScale = new Vector3(scale, scale, scale);
+
+        // mudar cor ao longo do tempo
+        float r = Mathf.Sin(Time.time) * 0.5f + 0.5f;
+        float g = Mathf.Sin(Time.time * 2) * 0.5f + 0.5f;
+        float b = Mathf.Sin(Time.time * 3) * 0.5f + 0.5f;
+
+        cubeRenderer.material.color = new Color(r, g, b);
+    }
+}
